7e528c7fd95cb07da0f30eb5c4162c69aa632572
Pavlo Sheremet <pashqa.sheremet@gmail.com>
2025-09-27 19:59:00 +0300

    save code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,95 @@
 import Section from "../Section/Section";
 import Container from "../Container/Container";
+import type { Photo } from "../../types/photo";
+import { getPhotos } from "../../services/photos";
+import { useState } from "react";
 
 export default function App() {
+  const [images, setImages] = useState<Photo[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  // images - App
+
+  // 1. get query from Form
+  // 2. http request
+  // 3. set images to state
+  // 4. pass images to Gallery List
+
+  const getQuery = async (query: string) => {
+    try {
+      setIsLoading(true);
+      setIsError(false);
+      const photos = await getPhotos(query);
+      setImages(photos);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <Section>
-        <Container>{/* Компоненти застосунку */}</Container>
-        Home page
+        <Container>
+          {/* FORM */}
+          <Form onSubmit={getQuery} />
+
+          {/* LOADER */}
+          {isLoading && <Loader />}
+          {isError && <div>Some error! Try again later</div>}
+
+          {/* GALLERY LIST */}
+          <GalleryList images={images} />
+        </Container>
       </Section>
     </>
   );
 }
+
+interface FormProps {
+  onSubmit: (query: string) => void;
+}
+
+const Form = ({ onSubmit }: FormProps) => {
+  const handleSubmit = (formData: FormData) => {
+    const query = formData.get("query") as string;
+
+    onSubmit(query);
+  };
+
+  return (
+    <form action={handleSubmit}>
+      <input type="text" name="query" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+interface GalleryListProps {
+  images: Photo[];
+}
+
+// 2. Gallery list
+const GalleryList = ({ images }: GalleryListProps) => {
+  return (
+    <ul>
+      {images.map((image) => {
+        return (
+          <li key={image.id}>
+            <img src={image.src.large} alt={image.alt} width={150} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+// 3. Loader
+//  компонент заглушка
+const Loader = () => {
+  return <div>Loading...</div>;
+};
+
+// викликати компоненти в App
--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import type { Photo } from "../types/photo";
 
 const API_KEY = "563492ad6f9170000100000108dc2880626e4436b3634ce1cf6b4d74";
 axios.defaults.baseURL = "https://api.pexels.com/v1/";
@@ -7,8 +8,18 @@ axios.defaults.params = {
   orientation: "landscape",
 };
 
-export const getPhotos = async (query) => {
-  const response = await axios.get(`search?query=${query}`);
+interface ResponseData {
+  photos: Photo[];
+}
+
+export const getPhotos = async (query: string): Promise<Photo[]> => {
+  const response = await axios.get<ResponseData>(`search?query=${query}`);
 
   return response.data.photos;
 };
+
+// export const getPhotoById = async (id: number) => {
+//   const response = await axios.get(`https://api.pexels.com/v1/photo/${id}`);
+
+//   return response.data;
+// };
